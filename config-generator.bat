0ea321d3fd8d298f054df73b5264ce7d399fa568
Gyrfalcon <46728024+Gyrfalc0n@users.noreply.github.com>
2021-05-05 13:11:58 +0200

    Create config-generator.bat

---
+++ config-generator.bat
@@ -0,0 +1,9 @@
+@echo off
+set output_path =<your output path here>
+set python_path =C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\python.exe
+pushd %output_path%
+IF EXIST "library.xml" (
+    del library.xml
+)
+%python_path% "nexusfont-generator.py"
+popd
