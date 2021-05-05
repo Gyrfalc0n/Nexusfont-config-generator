ebd053a7edcda5aae2ac848c4475a7416ee87e61
Gyrfalcon <46728024+Gyrfalc0n@users.noreply.github.com>
2021-05-05 13:23:14 +0200

    Update nexusfont-generator.py

--- nexusfont-generator.py
+++ nexusfont-generator.py
@@ -1,28 +1,28 @@
 import os
 from os import path
 
-def listdirs(rootdir):
-    f = open("library.xml", "w") # écraser le ficher
-    f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-    f.write("<library>\n")
-    f.write("<groups>\n")
-    f.write("<group name=\"Police Clients\" expanded=\"true\">\n")
+def generator(rootdir):
     for it in os.scandir(rootdir):
         if it.is_dir():
             text = "<folder>" + it.path + "</folder>\n"
-            f.write(text)
-            #listdirs(it) recursif pour afficher les sous dossiers
-    f.write("</group>\n")
-    f.write("<group name=\"Toutes les polices\" expanded=\"false\">\n")
-    text = "<folder>" + rootdir + "</folder>\n"
-    f.write(text)
-    f.write("</group>\n")
-    f.write("</groups>\n")
-    f.write("<sets/>\n")
-    f.write("</library>\n")
-    f.close()
+            #generator(it) uncomment this line to enable nexusfont group creation for any subfolder in your rootdir (reccursive)
+            return text
 
 # rootdir is your font directory, the folder where you have all your subfolders with font
-
 rootdir = "your_root_dir"
-listdirs(rootdir)
+
+f = open("library.xml", "w") # rewrite file
+f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+f.write("<library>\n")
+f.write("<groups>\n")
+f.write("<group name=\"Police Clients\" expanded=\"true\">\n")
+f.write(generator(rootdir))
+f.write("</group>\n")
+f.write("<group name=\"Toutes les polices\" expanded=\"false\">\n")
+text = "<folder>" + rootdir + "</folder>\n"
+f.write(text)
+f.write("</group>\n")
+f.write("</groups>\n")
+f.write("<sets/>\n")
+f.write("</library>\n")
+f.close()
