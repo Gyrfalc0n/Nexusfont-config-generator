4e1c022af71dbfad65c2bcc9b9c44af597b1c186
Gyrfalcon <46728024+Gyrfalc0n@users.noreply.github.com>
2021-05-05 13:46:13 +0200

    Update nexusfont-generator.py

--- nexusfont-generator.py
+++ nexusfont-generator.py
@@ -5,13 +5,13 @@ def generator(rootdir):
     for it in os.scandir(rootdir):
         if it.is_dir():
             text = "<folder>" + it.path + "</folder>\n"
-            #generator(it) uncomment this line to enable nexusfont group creation for any subfolder in your rootdir (reccursive)
+            #generator(it) uncomment this line to enable nexusfont group creation for any subfolder in your rootdir (recursive)
             f.write(text)
 
-# rootdir is your font directory, the folder where you have all your subfolders with font
-rootdir = "E:\\Nexus"
+# rootdir is your font directory, the folder where you have all your subfolders with font. Dont forget to escape \ with \\
+rootdir = "your root dir"
 
-f = open("library.xml", "w") # rewrite file
+f = open("library.xml", "w")
 f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
 f.write("<library>\n")
 f.write("<groups>\n")
