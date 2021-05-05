7ee104cbbc4c5fed53bedecc8cacd86a840f671e
Gyrfalcon <46728024+Gyrfalc0n@users.noreply.github.com>
2021-05-05 13:44:57 +0200

    Update config-generator.bat

--- config-generator.bat
+++ config-generator.bat
@@ -1,12 +1,14 @@
-@echo off
 Rem Your destination directory for library.xml file
-set output_path =<your output path here>
-Rem Your python3 path
-set python_path =<your python3 path here>
+set output_path =C:\Users\Enguerrand\Desktop
 pushd %output_path%
 IF EXIST "library.xml" (
     del library.xml
 )
-%python_path% "nexusfont-generator.py"
+Rem "Your python3 path" "python generator file"
+"E:\Users\Enguerrand\AppData\Local\Programs\Python\Python38-32\python.exe" "nexusfont.py"
 popd
-xcopy /s /y %output_path%\library.xml C:\Users\%USERNAME%\AppData\Roaming\nexusfont\
+Rem IF BASIC NEXUSFONT INSTALL
+Rem xcopy /s /y library.xml C:\Users\%USERNAME%\AppData\Roaming\nexusfont\
+Rem IF PORTABLE NEXUSFONT INSTALL
+xcopy /s /y library.xml path_to_nexusfont\userdata
+pause
